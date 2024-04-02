f11bb71b1547bdd9ed246856cbc6af1fc61c3511
zlkn <zolkin.u.a@gmail.com>
2024-04-02 16:38:57 +0300

    Add some instructions

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-mkdir -p $PWD/Personal || true
-mkdir -p $PWD/Workspace || true
-ln -sf $PWD/gitconfig $HOME/.gitconfig
-ln -sf $PWD/Personal/gitconfig $HOME/Personal/.gitconfig
