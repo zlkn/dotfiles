aa2d3b57755b354f6aefd4e76eec37f9b67027f7
zolkin.u.a <zolkin.u.a@gmail.com>
2024-05-11 18:59:46 +0300

    Fix colorscheme

--- vim/.config/nvim/lua/plugins/colorscheme.lua
+++ vim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,31 +1,46 @@
 return {
-	{
-		"nick-kadutskyi/vim-jb", -- My color theme (forked from devsjs/vim-js)
-		lazy = false,
-		name = "jb",
-		-- dev = true, -- theme is in dev but falls back to my public GitHub repo
-		init = function()
-			vim.g.jb_enable_italics = 1 -- Enables intalics
-			vim.g.jb_style = "light" -- JB defualt light theme
-		end,
-	},
-	{
-		"rmehri01/onenord.nvim",
-		lazy = false, -- make
-		name = "onenord",
-		opts = {
-			disable = {
-				background = true, -- Disable setting the background color
-			},
-		},
-	},
+	-- {
+	-- 	"nick-kadutskyi/vim-jb", -- My color theme (forked from devsjs/vim-js)
+	-- 	name = "jb",
+	-- 	-- dev = true, -- theme is in dev but falls back to my public GitHub repo
+	-- 	init = function()
+	-- 		vim.g.jb_enable_italics = 1 -- Enables intalics
+	-- 		vim.g.jb_style = "light" -- JB defualt light theme
+	-- 	end,
+	-- },
+	-- {
+	-- 	"rmehri01/onenord.nvim",
+	-- 	name = "onenord",
+	-- 	opts = {
+	-- 		disable = {
+	-- 			background = true, -- Disable setting the background color
+	-- 		},
+	-- 	},
+	-- },
 	{
 		"projekt0n/github-nvim-theme",
-		lazy = false,
-		priority = 1000,
+		name = "github",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+
 		config = function()
 			require("github-theme").setup({
+				options = {
+					theme_style = "light",
+					transparent = true,
+					styles = {
+						comments = "italic",
+						keywords = "bold",
+						types = "italic,bold",
+					},
+				},
 			})
 		end,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "github_light_high_contrast",
+		},
 	},
 }
