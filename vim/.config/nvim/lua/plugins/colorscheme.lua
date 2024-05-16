6d161de127bdc0ec8c971a1cffd7b0dfa3e285e2
zolkin.u.a <zolkin.u.a@gmail.com>
2024-05-16 06:43:15 +0300

    Make inlay info more visible

--- vim/.config/nvim/lua/plugins/colorscheme.lua
+++ vim/.config/nvim/lua/plugins/colorscheme.lua
@@ -19,26 +19,32 @@ return {
             syntax = {
               string = "#27745c",
               -- variable = "#A66F00",
-              -- builtin0 = "#0a3069", -- Builtin variable
-              -- builtin1 = pl.syntax.keyword, -- Builtin type
-              -- builtin2 = pl.syntax.constant, -- Builtin const
-              -- comment = pl.syntax.comment, -- Comment
+              -- builtin0 = "#bf6408", -- Builtin variable
+              -- builtin1 = "#bf6408", -- Builtin type
+              -- builtin2 = "#ff0101", -- Builtin const
+              -- comment = "#4fb8cc", -- Comment
               -- conditional = pl.syntax.keyword, -- Conditional and loop
-              -- const = pl.syntax.constant, -- Constants, imports and booleans
+              -- const = "#ff1010", -- Constants, imports and booleans
               -- dep = pal.scale.red[8], -- Deprecated
-              -- field = pl.syntax.constant, -- Field
+              field = "#1f1f1f", -- Field
               func = "#871094", -- Functions and Titles
               -- ident = spec.fg1, -- Identifiers
               keyword = "#0033b3", -- Keywords
-              number = "#a66f00", -- Numbers
+              number = "#1f1f1f", -- Numbers
               -- operator = pl.syntax.constant, -- Operators
               -- param = "#a66f00", -- Parameters
               -- preproc = pl.syntax.keyword, -- PreProc
               -- regex = pl.syntax.string, -- Regex
               -- statement = pl.syntax.keyword, -- Statements
               -- type = pl.syntax.variable, -- Types
-              -- tag = pl.syntax.entityTag, -- Tags
+              -- tag = "#ff1111", -- Tags
               -- variable = "#523c79", -- Variables
+            },
+            diag = {
+              -- error = pal.danger.fg,
+              -- warn = "#4fb8cc",
+              -- info = "#4fb8cc",
+              hint = "#4fb8cc",
             },
           },
         },
@@ -46,9 +52,27 @@ return {
     end,
   },
   {
+    "scottmckendry/cyberdream.nvim",
+    name = "cyberdream",
+    lazy = false,
+    priority = 1000,
+    --
+    config = function()
+      require("cyberdream").setup({
+        -- Enable transparent background
+        transparent = false,
+
+        -- Enable italics comments
+        italic_comments = true,
+        theme = { variant = "light" },
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "github_light",
+      -- colorscheme = "cyberdream",
     },
   },
 }
