07218ae067629315adac33e933419e1f533ec8d9
zolkin.u.a <zolkin.u.a@gmail.com>
2024-05-18 15:38:36 +0300

    play with lsp config

--- vim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ vim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,17 +1,18 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = { "HiPhish/nvim-ts-rainbow2" },
-    opts = function(_, opts)
-      opts.ensure_installed = {
-        "hcl",
-        "terraform",
-      }
-      opts.highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { "" }, -- list of language that will be disabled
-        additional_vim_regex_highlighting = true,
-      }
-    end,
-  },
-}
+-- return {
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     dependencies = { "HiPhish/nvim-ts-rainbow2" },
+--     opts = function(_, opts)
+--       opts.ensure_installed = {
+--         "hcl",
+--         "terraform",
+--       }
+--       opts.highlight = {
+--         enable = true, -- false will disable the whole extension
+--         disable = { "" }, -- list of language that will be disabled
+--         additional_vim_regex_highlighting = true,
+--       }
+--     end,
+--   },
+-- }
+return {}
