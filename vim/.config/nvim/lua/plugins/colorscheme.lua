d6af06595274f4b31c02e9649dfdb42593d3708f
zolkin.u.a <zolkin.u.a@gmail.com>
2024-07-26 00:13:59 +0300

    Fix github-theme version till nvim not v10

--- vim/.config/nvim/lua/plugins/colorscheme.lua
+++ vim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,8 @@
 return {
   {
     "projekt0n/github-nvim-theme",
-    name = "github",
+    tag = "v1.0.2",
+    name = "github-nvim-theme",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
 
