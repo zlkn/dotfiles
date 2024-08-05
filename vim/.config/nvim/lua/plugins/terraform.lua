a3b2f98c7a19a386c7244f2cb6304682814cbae0
zolkin.u.a <zolkin.u.a@gmail.com>
2024-08-05 15:59:33 +0300

    Disable terraform validate duo to misconfig

--- vim/.config/nvim/lua/plugins/terraform.lua
+++ vim/.config/nvim/lua/plugins/terraform.lua
@@ -46,8 +46,8 @@ return {
     optional = true,
     opts = {
       linters_by_ft = {
-        terraform = { "terraform_validate" },
-        tf = { "terraform_validate" },
+        -- terraform = { "terraform_validate" },
+        -- tf = { "terraform_validate" },
       },
     },
   },
