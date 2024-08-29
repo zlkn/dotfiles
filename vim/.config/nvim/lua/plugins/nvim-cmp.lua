e6a9bad4926e0ba0a9575297740df8cc18c44a9e
zolkin.u.a <zolkin.u.a@gmail.com>
2024-08-29 17:05:50 +0300

    Prettify cmp bubble

---
+++ vim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,17 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = { "hrsh7th/cmp-emoji" },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    -- table.insert(opts.sources, { name = "emoji" })
+  
+    local cmp_window = require "cmp.config.window"
+    opts.window = {
+      completion = cmp_window.bordered(),
+      documentation = cmp_window.bordered(),
+    }
+  end,
+
+
+}
+
