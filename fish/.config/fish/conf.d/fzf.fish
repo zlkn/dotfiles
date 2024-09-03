69dea8534ef47dc896775166a3b8d4a9b4f10725
zolkin.u.a <zolkin.u.a@gmail.com>
2024-09-03 16:58:39 +0300

    Add fzf colors

---
+++ fish/.config/fish/conf.d/fzf.fish
@@ -0,0 +1,4 @@
+set -gx FZF_DEFAULT_OPTS "--height=40% --layout=reverse --info=inline --border --margin=1 --padding=1 \
+--color=bg+:#f8f8f8,bg:#f8f8f8,spinner:#f0c674,hl:#c30771 \
+--color=fg:#2e3436,header:#10a778,info:#20a5ba,pointer:#20a5ba \
+--color=marker:#cc6666,fg+:#20a5ba,prompt:#81a2be,hl+:#c30771"
