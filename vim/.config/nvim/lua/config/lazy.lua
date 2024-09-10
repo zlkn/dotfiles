41d986b2613a9307881df8cc7869e5131038bfd3
zolkin.u.a <zolkin.u.a@gmail.com>
2024-09-10 10:09:58 +0300

    (nvim) Refactor LazyVim config

--- vim/.config/nvim/lua/config/lazy.lua
+++ vim/.config/nvim/lua/config/lazy.lua
@@ -20,11 +20,14 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.helm" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    -- { import = "lazyvim.plugins.extras.editor.navic" },
+
+
     -- { import = "lazyvim.plugins.extras.editor.illuminate" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.go" },
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
