7f2a30da1beacdec1fc3262bc9455a99b3bb454b
zolkin.u.a <zolkin.u.a@gmail.com>
2024-09-16 10:05:18 +0300

    (fish) chore python venv status

--- fish/.config/fish/conf.d/nai_plus.fish
+++ fish/.config/fish/conf.d/nai_plus.fish
@@ -3,6 +3,8 @@
 # * Current directory name
 # * Git branch and dirty state (if inside a git repo)
 
+
+
 function _git_branch_ahead
     set -l ahead (command git rev-list --count @{u}..HEAD 2> /dev/null)
     if not string match -qr '^\d+$' $ahead
@@ -21,7 +23,6 @@ function _git_branch_behind
     end
 end
 
-
 function _git_branch_name
     echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
@@ -30,23 +31,21 @@ function _git_dirty
     echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
-function fish_prompt
+function _git_info
+    set -l git_info ""
+
     set -l yellow (set_color yellow)
     set -l green (set_color green)
     set -l blue (set_color blue)
     set -l red (set_color red)
     set -l normal (set_color normal)
 
-    set -l cwd (basename (prompt_pwd))
-
-    echo -ns ' ' $cwd $normal
-
     if test (functions -q _git_branch_name) -a (set -q (_git_branch_name))
         # truncate branch name to 15 characters
         set -l git_branch "$(_git_branch_name)"
 
         if test (string length $git_branch) -gt 0
-            set git_branch "  $git_branch"
+            set git_branch " $git_branch"
         end
         if test (string length $git_branch) -gt 15
             set git_branch (string sub -s 1 -l 15 $git_branch)
@@ -56,6 +55,7 @@ function fish_prompt
         # Retrieve ahead and behind counts, ensuring they are numbers or fallback to 0
         set -l behind (_git_branch_behind)
         set -l ahead (_git_branch_ahead)
+        set -l status_color $green
 
         if test $ahead -gt 0 -a $behind -gt 0
             set status_color $red
@@ -67,8 +67,28 @@ function fish_prompt
             set status_color $green
         end
 
-        set git_info "$status_color$git_branch$normal "
+        set git_info " $status_color$git_branch$normal"
     end
 
-    echo -ns ' ' $git_info
+    echo $git_info
+
+end
+
+function _python_env
+    set -l color (set_color bryellow)
+    set -l normal (set_color normal)
+    if test -n "$VIRTUAL_ENV"
+        # set -l python_env (basename $VIRTUAL_ENV)
+        set -l python_version (python --version | awk '{print $2}')
+        echo -ns " $color $python_version$normal"
+    end
+    echo ""
+end
+
+function fish_prompt
+    set -l cwd (basename (prompt_pwd))
+    set -l git_info (_git_info)
+    set -l pytohn_env (_python_env)
+
+    echo -ns "$cwd$pytohn_env$git_info "
 end
