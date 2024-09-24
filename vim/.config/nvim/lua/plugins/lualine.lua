7d0bcc52fbe990c633d0971c3affbc9b52a567bb
zolkin.u.a <zolkin.u.a@gmail.com>
2024-09-24 09:05:32 +0300

    (vim) Fix background colors for lualine in all modes

--- vim/.config/nvim/lua/plugins/lualine.lua
+++ vim/.config/nvim/lua/plugins/lualine.lua
@@ -8,9 +8,9 @@ return {
       -- transparent background for lualine
       -- https://www.reddit.com/r/neovim/comments/zh4kc8/comment/jhekub8/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
       local auto_theme_custom = require('lualine.themes.auto')
-      auto_theme_custom.normal.c.bg = 'none'
-      auto_theme_custom.insert.c.bg = 'none'
-      auto_theme_custom.visual.c.bg = 'none'
+      for mode, _ in pairs(auto_theme_custom) do
+        auto_theme_custom[mode].c.bg = 'none'
+      end
 
       opts.options = {
         theme = auto_theme_custom,
