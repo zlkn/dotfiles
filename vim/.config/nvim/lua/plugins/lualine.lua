d359ac2e6232448586bfcbb1b759fee14f3b2411
zolkin.u.a <zolkin.u.a@gmail.com>
2024-09-30 16:19:41 +0300

    (nvim) Try to move on top file related info

--- vim/.config/nvim/lua/plugins/lualine.lua
+++ vim/.config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,11 @@
+local function yaml_schema()
+    local schema = require("yaml-companion").get_buf_schema(0)
+    if schema.result[1].name == "none" then
+        return ""
+    end
+    return schema.result[1].name
+end
+
 return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
@@ -6,27 +14,37 @@ return {
         -- transparent background for lualine
         -- https://www.reddit.com/r/neovim/comments/zh4kc8/comment/jhekub8/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
         local auto_theme_custom = require("lualine.themes.auto")
+        local icons = LazyVim.config.icons
         for mode, _ in pairs(auto_theme_custom) do
             auto_theme_custom[mode].c.bg = "none"
         end
 
         opts.options = {
             theme = auto_theme_custom,
-            component_separators = { left = "", right = "" },
+            component_separators = { left = "", right = "" },
             section_separators = { left = "", right = "" },
         }
         -- opts.sections.lualine_z = {'Schema:', function() return get_schema() end }
+        -- opts.sections.lualine_c = {}
         opts.sections.lualine_z = {
             "encoding",
-            {
-                function()
-                    local schema = require("yaml-companion").get_buf_schema(0)
-                    if schema.result[1].name == "none" then
-                        return ""
-                    end
-                    return schema.result[1].name
-                end,
-            },
+        }
+        opts.tabline = {
+            -- lualine_c = {
+            --     LazyVim.lualine.root_dir(),
+            --     {
+            --         "diagnostics",
+            --         symbols = {
+            --             error = icons.diagnostics.Error,
+            --             warn = icons.diagnostics.Warn,
+            --             info = icons.diagnostics.Info,
+            --             hint = icons.diagnostics.Hint,
+            --         },
+            --     },
+            --     { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            --     { yaml_schema() },
+            --     { LazyVim.lualine.pretty_path() },
+            -- },
         }
     end,
 }
