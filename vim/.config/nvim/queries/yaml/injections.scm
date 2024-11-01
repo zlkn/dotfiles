6adb151bb1246c81eca3044c15a88bddb77dfa5e
zolkin.u.a <zolkin.u.a@gmail.com>
2024-11-01 16:35:07 +0200

    (nvim) format query

--- vim/.config/nvim/queries/yaml/injections.scm
+++ vim/.config/nvim/queries/yaml/injections.scm
@@ -2,31 +2,24 @@
 
 ; Support Idea-style injection: # language=yaml
 ; Inject language toplevel yaml node
-(
-  (comment) @injection.language
+((comment) @injection.language
   (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
   (block_mapping_pair
     value: (block_node
       (block_scalar) @injection.content
       (#offset! @injection.content 0 1 0 0)
-    )
-  )
-)
+      )))
 
-; Support Idea-style injection: # language=yaml
 ; Inject language in yaml body
 (block_mapping_pair
   (comment) @injection.language
   (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
   value: (block_node
     (block_mapping
-    (block_mapping_pair
-      value: (block_node
-        (block_scalar) @injection.content
-        (#offset! @injection.content 0 1 0 0)
-        )
-      )
-    )
-  )
-)
+      (block_mapping_pair
+        value: (block_node
+          (block_scalar) @injection.content
+          (#offset! @injection.content 0 1 0 0)
+          )))))
+
 
