9119ad9f5987663ad89fea12e8cca2d567fa83cc
zolkin.u.a <zolkin.u.a@gmail.com>
2024-11-05 10:49:51 +0200

    (nvim) format extral query

--- vim/.config/nvim/queries/yaml/injections.scm
+++ vim/.config/nvim/queries/yaml/injections.scm
@@ -3,23 +3,21 @@
 ; Support Idea-style injection: # language=yaml
 ; Inject language at toplevel yaml node
 ((comment) @injection.language
- (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
- . (block_mapping_pair
-   value: (block_node
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  .
+  (block_mapping_pair
+    value: (block_node
       (block_scalar) @injection.content
-      (#offset! @injection.content 0 1 0 0)
-      )))
+      (#offset! @injection.content 0 1 0 0))))
 
 ; Inject language in yaml body
 (block_mapping_pair
   (comment) @injection.language
   (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
   value: (block_node
-     (block_mapping
-     . (block_mapping_pair
+    (block_mapping
+      .
+      (block_mapping_pair
         value: (block_node
           (block_scalar) @injection.content
-          (#offset! @injection.content 0 1 0 0)
-          )))))
-
-
+          (#offset! @injection.content 0 1 0 0))))))
