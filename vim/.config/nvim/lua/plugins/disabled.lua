e4f0a929faadbe1e152e0dffddaf162e0aa8a9ba
zolkin.u.a <zolkin.u.a@gmail.com>
2024-11-18 17:07:52 +0200

    (nvim) play around disable some plugins

--- vim/.config/nvim/lua/plugins/disabled.lua
+++ vim/.config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
     -- { "nvimdev/dashboard-nvim", enabled = false },
-    { "akinsho/bufferline.nvim", enabled = false },
+    -- { "akinsho/bufferline.nvim", enabled = false },
+    -- { "stevearc/conform.nvim", enabled },
 }
