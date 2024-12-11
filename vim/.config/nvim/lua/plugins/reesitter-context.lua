a8d8ee3a79ee855eeafa8af78181696b50e6d2c9
zolkin.u.a <zolkin.u.a@gmail.com>
2024-12-11 09:23:26 +0200

    (nvim) chore ui

--- vim/.config/nvim/lua/plugins/reesitter-context.lua
+++ vim/.config/nvim/lua/plugins/reesitter-context.lua
@@ -7,6 +7,6 @@ return {
         require("treesitter-context").setup(opts)
 
         -- Set the highlight for TreesitterContext after the plugin is loaded
-        vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#f1f1f1" })
+        vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#e7eaf0" })
     end,
 }
