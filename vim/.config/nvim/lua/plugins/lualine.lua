cb92db944ba6e2a39f3310c141ac04d0f1125ce2
zolkin.u.a <zolkin.u.a@gmail.com>
2024-12-11 15:29:44 +0200

    (nvim) Delete bullshit

--- vim/.config/nvim/lua/plugins/lualine.lua
+++ vim/.config/nvim/lua/plugins/lualine.lua
@@ -18,71 +18,11 @@ return {
             section_separators = { left = "", right = "" },
             always_show_tabline = true,
         }
-        opts.sections.lualine_c = {
-            {
-                "diff",
-                symbols = {
-                    added = icons.git.added,
-                    modified = icons.git.modified,
-                    removed = icons.git.removed,
-                },
-                source = function()
-                    local gitsigns = vim.b.gitsigns_status_dict
-                    if gitsigns then
-                        return {
-                            added = gitsigns.added,
-                            modified = gitsigns.changed,
-                            removed = gitsigns.removed,
-                        }
-                    end
-                end,
-            },
-            { LazyVim.lualine.pretty_path() },
-        }
-        -- opts.sections.lualine_c = {}
-        opts.sections.lualine_x = {
-            -- stylua: ignore
-            {
-                function() return require("noice").api.status.command.get() end,
-                cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-                color = function() return LazyVim.ui.fg("Statement") end,
-            },
-            -- stylua: ignore
-            {
-                function() return require("noice").api.status.mode.get() end,
-                cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-                color = function() return LazyVim.ui.fg("Constant") end,
-            },
-            -- stylua: ignore
-            {
-                function() return "  " .. require("dap").status() end,
-                cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-                color = function() return LazyVim.ui.fg("Debug") end,
-            },
-            -- stylua: ignore
-            {
-                require("lazy.status").updates,
-                cond = require("lazy.status").has_updates,
-                color = function() return LazyVim.ui.fg("Special") end,
-            },
-        }
-        opts.sections.lualine_z = { { "encoding" }, { extensions = { "neo-tree", "lazy" } } }
 
-        opts.tabline = {
-            lualine_b = {
-                LazyVim.lualine.root_dir(),
-                {
-                    "diagnostics",
-                    symbols = {
-                        error = icons.diagnostics.Error,
-                        warn = icons.diagnostics.Warn,
-                        info = icons.diagnostics.Info,
-                        hint = icons.diagnostics.Hint,
-                    },
-                },
-                { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-                { LazyVim.lualine.pretty_path() },
-            },
+        opts.sections.lualine_z = {
+            { "encoding" },
+            { extensions = { "neo-tree", "lazy" } },
         }
+        opts.tabline = {}
     end,
 }
