293771156eecb1ca005b081045774e7c949d27b7
zolkin.u.a <zolkin.u.a@gmail.com>
2024-12-16 09:58:42 +0200

    (nvim) disable tree sitter context

--- vim/.config/nvim/lua/plugins/reesitter-context.lua
+++ vim/.config/nvim/lua/plugins/reesitter-context.lua
@@ -1,12 +1,13 @@
 return {
-    "nvim-treesitter/nvim-treesitter-context",
-    opts = {
-        max_lines = 0,
-    },
-    config = function(_, opts)
-        require("treesitter-context").setup(opts)
-
-        -- Set the highlight for TreesitterContext after the plugin is loaded
-        vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#e7eaf0" })
-    end,
+    -- "nvim-treesitter/nvim-treesitter-context",
+    -- opts = {
+    --     max_lines = 0,
+    -- },
+    -- config = function(_, opts)
+    --     require("treesitter-context").setup(opts)
+    --
+    --     -- Set the highlight for TreesitterContext after the plugin is loaded
+    --     -- vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#e7eaf0" })
+    --     vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#f8f8f8" })
+    -- end,
 }
