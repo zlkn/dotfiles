5e61b0e010122d2738f3a8e3bd3a61092d1f562f
zolkin.u.a <zolkin.u.a@gmail.com>
2024-12-17 00:26:13 +0200

    (fish) use fzf instead of builtin search

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,12 @@ if status is-interactive
     alias ghe='gh copilot explain -t shell'
 end
 
+function __fzf_history_search
+    commandline (history | fzf)
+    commandline -f execute
+end
+
 function fish_user_key_bindings
     bind -k nul forward-word
+    bind \cr __fzf_history_search
 end
