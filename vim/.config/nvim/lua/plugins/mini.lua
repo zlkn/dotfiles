0251a76d231cc8227e670208ebd4c1d75fc870fe
zolkin.u.a <zolkin.u.a@gmail.com>
2024-12-18 19:12:22 +0200

    (nvim) Add mini.cursorword for hightlight current word in buffer

---
+++ vim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "echasnovski/mini.cursorword",
+        version = false,
+        name = "mini-cursorword",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 100, -- make sure to load this before all the other start plugins
+    },
+}
