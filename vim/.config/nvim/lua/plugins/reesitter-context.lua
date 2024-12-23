c93b3da4726741d5dcd1ffc72f8616bee0002a08
zolkin.u.a <zolkin.u.a@gmail.com>
2024-12-23 18:02:07 +0200

    (nvim) Enable treesitter-context instead of dropbar plugin

--- vim/.config/nvim/lua/plugins/reesitter-context.lua
+++ vim/.config/nvim/lua/plugins/reesitter-context.lua
@@ -1,13 +1,13 @@
 return {
-    -- "nvim-treesitter/nvim-treesitter-context",
-    -- opts = {
-    --     max_lines = 0,
-    -- },
-    -- config = function(_, opts)
-    --     require("treesitter-context").setup(opts)
-    --
-    --     -- Set the highlight for TreesitterContext after the plugin is loaded
-    --     -- vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#e7eaf0" })
-    --     vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#f8f8f8" })
-    -- end,
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+        max_lines = 0,
+        min_window_height = 30,
+        mode = "topline",
+    },
+    config = function(_, opts)
+        require("treesitter-context").setup(opts)
+        -- Set the highlight for TreesitterContext after the plugin is loaded
+        vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#d9d9d9" })
+    end,
 }
