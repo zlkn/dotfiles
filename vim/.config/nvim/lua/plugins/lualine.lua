b1bee534979e77e957eb7e04e7cfb68586072124
zolkin.u.a <zolkin.u.a@gmail.com>
2024-12-23 18:00:55 +0200

    (nvim) Move file status to tabline

--- vim/.config/nvim/lua/plugins/lualine.lua
+++ vim/.config/nvim/lua/plugins/lualine.lua
@@ -24,6 +24,21 @@ return {
             { "encoding" },
             { extensions = { "neo-tree", "lazy" } },
         }
-        opts.tabline = {}
+        opts.tabline = {
+            lualine_c = {
+                LazyVim.lualine.root_dir(),
+                {
+                    "diagnostics",
+                    symbols = {
+                        error = icons.diagnostics.Error,
+                        warn = icons.diagnostics.Warn,
+                        info = icons.diagnostics.Info,
+                        hint = icons.diagnostics.Hint,
+                    },
+                },
+                { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+                { LazyVim.lualine.pretty_path() },
+            },
+        }
     end,
 }
