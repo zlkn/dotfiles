5e0918e0c685ceeb177f486a79f7ed50df50c9b5
zolkin.u.a <zolkin.u.a@gmail.com>
2024-12-23 18:02:55 +0200

    (nvim) Move command pallete to left bottom corner

--- vim/.config/nvim/lua/plugins/noice.lua
+++ vim/.config/nvim/lua/plugins/noice.lua
@@ -7,6 +7,28 @@ return {
         },
         presets = {
             lsp_doc_border = true,
+            command_palette = true,
+        },
+        views = {
+            cmdline_popupmenu = {
+                relative = "editor",
+                position = {
+                    row = "97%",
+                    col = "1%",
+                },
+                size = {
+                    width = 60,
+                    height = "auto",
+                    max_height = 15,
+                },
+                -- border = {
+                --     style = "none",
+                --     padding = { 0, 3 },
+                -- },
+                win_options = {
+                    winhighlight = { NormalFloat = "NormalFloat", FloatBorder = "NoiceCmdlinePopupBorder" },
+                },
+            },
         },
     },
 }
