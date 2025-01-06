1155c30d880ab9636061bcbc32421cfb77aa301d
zolkin.u.a <zolkin.u.a@gmail.com>
2025-01-06 15:56:42 +0200

    (wezterm) Use named colors in colorscheme

--- wezterm/.config/wezterm/colors.lua
+++ wezterm/.config/wezterm/colors.lua
@@ -1,6 +1,6 @@
 local pallete = {
     ansi = {
-        balack = "#313131",
+        black = "#313131",
         red = "#c30771",
         green = "#10a778",
         yellow = "#a66f00",
@@ -10,7 +10,7 @@ local pallete = {
         white = "#d9d9d9",
     },
     brights = {
-        balack = "#212121",
+        black = "#212121",
         red = "#fb007a",
         green = "#5fd7af",
         yellow = "#f39c12",
@@ -19,37 +19,53 @@ local pallete = {
         cyan = "#4fb8cc",
         white = "#a1a1a1",
     },
+    extra = {
+        cornflowerBlue = "#b6d6fd",
+        lightGray = "#f8f8f8",
+        darkGray = "#424242",
+        pureWhite = "#ffffff",
+    },
 }
 
+local function get_values(t)
+    local values = {}
+    for _, value in pairs(t) do
+        table.insert(values, value)
+    end
+    print(values)
+    return values
+end
+
 local colors = {
-    foreground = "#313113",
-    background = "#f8f8f8",
-    cursor_bg = "#20bbfc",
-    cursor_fg = "#424242",
-    cursor_border = "#20bbfc",
-    selection_bg = "#b6d6fd",
-    selection_fg = "#e13131",
-    scrollbar_thumb = "#313131",
+
+    indexed = {},
+
+    foreground = pallete.ansi.black,
+    background = pallete.extra.lightGray,
+    cursor_bg = pallete.brights.blue,
+    cursor_fg = pallete.extra.darkGray,
+    cursor_border = pallete.brights.blue,
+    selection_bg = pallete.extra.cornflowerBlue,
+    selection_fg = pallete.ansi.black,
+    scrollbar_thumb = pallete.ansi.black,
     split = pallete.brights.white,
 
     -- Before 16 colors, there were 8 colors: black, red, green, yellow, blue, magenta, cyan, and white.
     -- The other 8 colors were added as bright variants of these
-    ansi = { "#313131", "#c30771", "#10a778", "#a66f00", "#008ec4", "#523c79", "#20a5ba", "#d9d9d9" },
-    brights = { "#212121", "#fb007a", "#5fd7af", "#f39c12", "#20bbfc", "#6855de", "#4fb8cc", "#a1a1a1" },
+    ansi = get_values(pallete.ansi),
+    brights = get_values(pallete.brights),
 
-    indexed = {},
+    copy_mode_active_highlight_bg = { Color = pallete.extra.darkGray },
+    copy_mode_active_highlight_fg = { Color = pallete.ansi.magenta },
+    copy_mode_inactive_highlight_bg = { Color = pallete.ansi.green },
+    copy_mode_inactive_highlight_fg = { AnsiColor = pallete.extra.pureWhite },
 
-    copy_mode_active_highlight_bg = { Color = "#424242" },
-    copy_mode_active_highlight_fg = { Color = "#523c79" },
-    copy_mode_inactive_highlight_bg = { Color = "#52ad70" },
-    copy_mode_inactive_highlight_fg = { AnsiColor = "White" },
+    quick_select_label_bg = { Color = pallete.ansi.green },
+    quick_select_label_fg = { Color = pallete.extra.pureWhite },
+    quick_select_match_bg = { Color = pallete.extra.cornflowerBlue },
+    quick_select_match_fg = { Color = pallete.extra.darkGray },
 
-    quick_select_label_bg = { Color = "peru" },
-    quick_select_label_fg = { Color = "#ffffff" },
-    quick_select_match_bg = { Color = "#b6d6fd" },
-    quick_select_match_fg = { Color = "#424242" },
-
-    tab_bar = { inactive_tab_edge = "#f8f8f8" },
+    tab_bar = { inactive_tab_edge = pallete.extra.lightGray },
 }
 
 return {
