0c0410fc69d2c63c7cbfa26ed0ba7db9f9f9ead1
zolkin.u.a <zolkin.u.a@gmail.com>
2025-01-14 23:37:36 +0200

    (nvim/feature) Support blue cursor

--- vim/.config/nvim/lua/config/autocmds.lua
+++ vim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    once = true,
+    callback = function(args)
+        vim.cmd([[ hi! Cursor guibg=NONE guifg=NONE gui=reverse ]])
+    end,
+})
