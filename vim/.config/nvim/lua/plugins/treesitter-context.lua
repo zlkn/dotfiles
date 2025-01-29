4af2115a79a160b4388241ba249c21ec5cb2d8c8
zolkin.u.a <zolkin.u.a@gmail.com>
2025-01-29 15:15:54 +0200

    feat(nvim) Chore treesitter context colors

--- vim/.config/nvim/lua/plugins/treesitter-context.lua
+++ vim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -8,6 +8,8 @@ return {
     config = function(_, opts)
         require("treesitter-context").setup(opts)
         -- Set the highlight for TreesitterContext after the plugin is loaded
-        vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#d9d9d9" })
+        vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#e9e9e9" })
+        vim.api.nvim_set_hl(0, "TreesitterContextBottom", { blend = 32, underline = true, sp = "#d9d9d9" })
+        vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { blend = 32, underline = true, sp = "#d9d9d9" })
     end,
 }
