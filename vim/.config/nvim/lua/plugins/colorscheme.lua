ca298919d37efcdf37c1e6da25e2100cc18c383e
zolkin.u.a <zolkin.u.a@gmail.com>
2025-02-11 22:22:35 +0200

    feat(nvim) change black in colorscheme

--- vim/.config/nvim/lua/plugins/colorscheme.lua
+++ vim/.config/nvim/lua/plugins/colorscheme.lua
@@ -24,7 +24,7 @@ return {
                 specs = {
                     github_light = {
                         syntax = {
-                            bracket = "#212121",
+                            bracket = "#313131",
                             builtin0 = "#c30771",
                             builtin1 = "#008ec4",
                             builtin2 = "#523c79",
@@ -32,16 +32,15 @@ return {
                             conditional = "#cf222e",
                             const = "#0550ae",
                             dep = "#82071e",
-                            field = "#212121",
+                            field = "#313131",
                             func = "#871094",
-                            ident = "#212121",
+                            ident = "#313131",
                             keyword = "#0550ae",
-                            number = "#212121",
+                            number = "#313131",
                             operator = "#0550ae",
                             -- param = "#008ec4",
                             -- param = "#20B2AA",
                             param = "#007474",
-                            -- param = "#0a3069",
                             -- param = "#20bbfc",
                             preproc = "#cf222e",
                             regex = "#0a3069",
@@ -51,7 +50,7 @@ return {
                             -- string = "#10a778",
                             tag = "#0055c4",
                             type = "#1f1f1f",
-                            variable = "#212121",
+                            variable = "#313131",
                         },
                         diag = {
                             -- error = pal.danger.fg,
