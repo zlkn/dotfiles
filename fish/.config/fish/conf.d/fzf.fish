4ca0f94621a464d9f0f38944701a1da15929b686
zolkin.u.a <zolkin.u.a@gmail.com>
2025-02-19 09:52:01 +0200

    feat(fzf) match background color

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -2,7 +2,8 @@
 # ‘batcat’ because of a file name clash with another Debian package.
 
 # set -gx FZF_DEFAULT_OPTS "--height=40% --layout=reverse --info=inline --border --margin=1 --padding=1 --bind='F2:toggle-preview' \
-set -gx FZF_DEFAULT_OPTS "--height=60% --layout=reverse --info=inline --bind='F2:toggle-preview' \
---color=bg+:#f1f1f1,bg:#f1f1f1,spinner:#f0c674,hl:#c30771 \
+set -gx FZF_DEFAULT_OPTS "--multi \
+--height=60% --layout=reverse --info=inline --bind='F2:toggle-preview' \
+--color=bg+:#f4f5f5,bg:#f4f5f5,spinner:#f0c674,hl:#c30771 \
 --color=fg:#2e3436,header:#10a778,info:#20a5ba,pointer:#20a5ba \
 --color=marker:#cc6666,fg+:#20a5ba,prompt:#81a2be,hl+:#c30771"
