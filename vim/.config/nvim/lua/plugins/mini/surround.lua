aed39073d1d2261cf617772598f46917ef2f759f
zolkin.u.a <zolkin.u.a@gmail.com>
2025-03-05 17:41:29 +0200

    feat(nvim/surround) Add config

--- vim/.config/nvim/lua/plugins/mini/surround.lua
+++ vim/.config/nvim/lua/plugins/mini/surround.lua
@@ -1,10 +1,22 @@
-MiniDeps.add({ source = 'echasnovski/mini.surround', checkout = 'stable' })
-MiniDeps.later(
-	function()
-		local surround = require('mini.surround')
-		surround.setup({
-			  -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
-			  highlight_duration = 500,
-		})
-	end
-)
+MiniDeps.add({ source = "echasnovski/mini.surround", checkout = "stable" })
+MiniDeps.later(function()
+    local surround = require("mini.surround")
+    surround.setup({
+        -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+        highlight_duration = 500,
+
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+            add = "sa", -- Add surrounding in Normal and Visual modes
+            delete = "sd", -- Delete surrounding
+            find = "sf", -- Find surrounding (to the right)
+            find_left = "sF", -- Find surrounding (to the left)
+            highlight = "sh", -- Highlight surrounding
+            replace = "sr", -- Replace surrounding
+            update_n_lines = "sn", -- Update `n_lines`
+
+            suffix_last = "l", -- Suffix to search with "prev" method
+            suffix_next = "n", -- Suffix to search with "next" method
+        },
+    })
+end)
