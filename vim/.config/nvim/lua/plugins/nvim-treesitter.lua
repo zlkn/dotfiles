998ff13a92aa10f8f23ec4b53cc4390511081a33
zolkin.u.a <zolkin.u.a@gmail.com>
2025-03-05 17:11:01 +0200

    Use mini.later for treesitter

--- vim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ vim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,22 +1,30 @@
 local add = MiniDeps.add
 
 -- Add to current session (install if absent)
-add({
-  source = 'neovim/nvim-lspconfig',
-  -- Supply dependencies near target plugin
-  depends = { 'williamboman/mason.nvim' },
-})
+-- add({
+--     source = "neovim/nvim-lspconfig",
+--     -- Supply dependencies near target plugin
+--     depends = { "williamboman/mason.nvim" },
+-- })
 
 add({
-  source = 'nvim-treesitter/nvim-treesitter',
-  -- Use 'master' while monitoring updates in 'main'
-  checkout = 'master',
-  monitor = 'main',
-  -- Perform action after every checkout
-  hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+    source = "nvim-treesitter/nvim-treesitter",
+    -- Use 'master' while monitoring updates in 'main'
+    checkout = "master",
+    monitor = "main",
+    -- Perform action after every checkout
+    hooks = {
+        post_checkout = function()
+            vim.cmd("TSUpdate")
+        end,
+    },
 })
--- Possible to immediately execute code which depends on the added plugin
-require('nvim-treesitter.configs').setup({
-  ensure_installed = { 'lua', 'vimdoc' },
-  highlight = { enable = true },
-})
+MiniDeps.later(function()
+    -- Possible to immediately execute code which depends on the added plugin
+    require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "vimdoc", "yaml" },
+        highlight = { enable = true },
+    })
+
+    vim.keymap.set({ "<c-space>", desc = "Increment Selection", mode = { "x", "n" } })
+end)
