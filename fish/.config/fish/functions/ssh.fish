1e92506f1851562c838cf2275a787af52ad0dd5c
zolkin.u.a <zolkin.u.a@gmail.com>
2025-03-07 17:52:27 +0200

    feature(fish) use wezterm ssh wrapper

---
+++ fish/.config/fish/functions/ssh.fish
@@ -0,0 +1,3 @@
+# function ssh --wraps='wezterm cli spawn --domain-name SSH:HOSTNAME' --description 'alias ssh=wezterm cli spawn --domain-name SSH:HOSTNAME'
+#     wezterm cli spawn --domain-name SSH:$argv
+# end
