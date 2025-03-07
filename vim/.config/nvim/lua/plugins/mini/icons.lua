1407f9367a14cc834599c9b1708238fcc13e133b
zolkin.u.a <zolkin.u.a@gmail.com>
2025-03-07 16:58:38 +0200

    feat(nvim) icons styla

--- vim/.config/nvim/lua/plugins/mini/icons.lua
+++ vim/.config/nvim/lua/plugins/mini/icons.lua
@@ -1,8 +1,5 @@
-MiniDeps.add({ source = 'echasnovski/mini.icons' })
-MiniDeps.later(
-function()
-	local icons = require('mini.icons')
-	icons.setup({})
-end
-)
-
+MiniDeps.add({ source = "echasnovski/mini.icons" })
+MiniDeps.later(function()
+    local icons = require("mini.icons")
+    icons.setup({})
+end)
