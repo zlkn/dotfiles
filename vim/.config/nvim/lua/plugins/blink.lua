781897aba5a19905e39cc932378537602af3be04
zolkin.u.a <zolkin.u.a@gmail.com>
2025-04-04 20:10:56 +0300

    feature(nvim) blink cmp ui

--- vim/.config/nvim/lua/plugins/blink.lua
+++ vim/.config/nvim/lua/plugins/blink.lua
@@ -13,8 +13,14 @@ MiniDeps.later(function()
         completion = {
             documentation = {
                 auto_show = true,
+                auto_show_delay_ms = 500,
+                border = "rounded",
+                window = {
+                    border = "rounded",
+                },
             },
             menu = {
+                border = "rounded",
                 draw = {
                     treesitter = { "lsp" },
                 },
@@ -28,4 +34,8 @@ MiniDeps.later(function()
             nerd_font_variant = "normal",
         },
     })
+
+    vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { fg = "#d1d1d1" })
+    vim.api.nvim_set_hl(0, "BlinkCmpMenu", { fg = "none", bg = "none" })
+    vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", { fg = "#d1d1d1" })
 end)
