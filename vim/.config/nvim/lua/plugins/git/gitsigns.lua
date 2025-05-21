b91f5c887541905d3171f9b1651c826a3985e480
zolkin.u.a <zolkin.u.a@gmail.com>
2025-05-21 09:15:02 +0300

    feat(nvim) disable by default current_line_blame

--- vim/.config/nvim/lua/plugins/git/gitsigns.lua
+++ vim/.config/nvim/lua/plugins/git/gitsigns.lua
@@ -2,7 +2,7 @@ MiniDeps.add("lewis6991/gitsigns.nvim")
 MiniDeps.later(function()
     local gitsigns = require("gitsigns")
     gitsigns.setup({
-        current_line_blame = true,
+        current_line_blame = false,
     })
     -- vim.api.nvim_set_keymap("n", "<Space>bl", [[<Cmd>lua require"gitsigns".blame_line()<CR>]], { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<Space>bl", [[<Cmd>:Gitsigns toggle_current_line_blame <CR>]], { noremap = true, silent = true })
