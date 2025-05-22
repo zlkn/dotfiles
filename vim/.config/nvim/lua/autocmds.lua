58e6318788366a04baad982b7ec441a317977946
zolkin.u.a <zolkin.u.a@gmail.com>
2025-05-22 12:59:49 +0300

    feature(nvim) save cursor position in file

--- vim/.config/nvim/lua/autocmds.lua
+++ vim/.config/nvim/lua/autocmds.lua
@@ -1,5 +1,3 @@
--- This file is automatically loaded by lazyvim.config.init.
-
 local function augroup(name)
     return vim.api.nvim_create_augroup("ag_" .. name, { clear = true })
 end
@@ -42,5 +40,17 @@ vim.api.nvim_create_autocmd("VimEnter", {
             bg = "none",
             reverse = true,
         })
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = general_settings,
+    desc = "remember last cursor place",
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
     end,
 })
