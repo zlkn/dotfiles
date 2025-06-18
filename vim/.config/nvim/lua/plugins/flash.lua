1cd92b767316720df99081f70a7bb0fd26de1fa2
zolkin.u.a <zolkin.u.a@gmail.com>
2025-06-18 12:39:57 +0300

    feat(nvim) use f for activate flash jump mode

--- vim/.config/nvim/lua/plugins/flash.lua
+++ vim/.config/nvim/lua/plugins/flash.lua
@@ -1,6 +1,6 @@
 MiniDeps.add({ source = "folke/flash.nvim" })
 MiniDeps.later(function()
-    vim.keymap.set("n", "S", function()
+    vim.keymap.set("n", "f", function()
         require("flash").jump()
     end, { desc = "Start flash jump mode", remap = true, silent = true })
 
