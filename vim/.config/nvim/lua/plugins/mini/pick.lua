23116030f238e64c7ae1df5333cad07086864040
zolkin.u.a <zolkin.u.a@gmail.com>
2025-06-26 13:46:56 +0300

    feature(nvim) Add yaml keytrail fzf picker

--- vim/.config/nvim/lua/plugins/mini/pick.lua
+++ vim/.config/nvim/lua/plugins/mini/pick.lua
@@ -3,10 +3,12 @@ MiniDeps.later(function()
     require("mini.pick").setup({
         window = { prompt_prefix = " " },
     })
+    MiniPick.registry.yaml_keytrail = M.yaml_keytrail
 
     vim.keymap.set("n", "<leader><leader>", function()
         MiniPick.builtin.files()
     end, { desc = "Files in rootdir" })
+
     vim.keymap.set("n", "<leader>/", function()
         MiniPick.builtin.grep_live()
     end, { desc = "Live grep" })
@@ -17,7 +19,8 @@ MiniDeps.later(function()
         MiniPick.builtin.files({ items = { "foo", "bar" } })
     end, { desc = "Changed files " })
 
-    vim.keymap.set("n", "<leader>pgm", ":Pick git_files scope='modified'<CR>", { desc = "Find modified files" })
+    vim.keymap.set("n", "<leader>pgm", ":Pick git_files scope='modified'<CR>", { desc = "Git modified files" })
+    vim.keymap.set("n", "<leader>pgh", ":Pick git_hunks", { desc = "Git hunks" })
     vim.keymap.set("n", "<leader>pr", ":Pick resume<CR>", { desc = "Resume last search" })
     vim.keymap.set("n", "<leader>pd", ":Pick diagnostic<CR>", { desc = "Diagnostics" })
 
@@ -29,3 +32,28 @@ MiniDeps.later(function()
 
     vim.keymap.set("n", "<leader>pp", ":echo 'Not Implemeted'<CR>", { desc = "Mock for jq over current buffer" })
 end)
+
+M = {}
+M.yaml_keytrail = function(_, opts)
+    local items = require("test").get_all_paths()
+    table.sort(items)
+
+    local function choose(item)
+        print("Choose item: " .. item)
+    end
+
+    local function preview(bufnr, item)
+        return nil
+    end
+
+    -- 4. Assemble source
+    local source = {
+        name = "YamlKeytrail",
+        items = items,
+        choose = choose,
+        preview = preview,
+    }
+
+    opts = vim.tbl_deep_extend("force", { source = source }, opts or {})
+    return MiniPick.start(opts)
+end
