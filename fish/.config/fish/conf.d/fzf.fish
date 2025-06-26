5e3950d1e2c8fe04450e8162c5bed542a3e82f71
zolkin.u.a <zolkin.u.a@gmail.com>
2025-06-26 13:46:39 +0300

    feat(fzf)imrove highlight

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -4,6 +4,5 @@
 # set -gx FZF_DEFAULT_OPTS "--height=40% --layout=reverse --info=inline --border --margin=1 --padding=1 --bind='F2:toggle-preview' \
 set -gx FZF_DEFAULT_OPTS "--multi \
 --height=60% --layout=reverse --info=inline --bind='F2:toggle-preview' \
---color=bg+:#f4f5f5,bg:#f4f5f5,spinner:#f0c674,hl:#c30771 \
---color=fg:#2e3436,header:#10a778,info:#20a5ba,pointer:#20a5ba \
---color=marker:#cc6666,fg+:#20a5ba,prompt:#81a2be,hl+:#c30771"
+--color=bg+:#e1e1e1,bg:-1,fg:#313131,fg+:#1f1f1f,hl:#871094,hl+:#871094 \
+--color=gutter:-1,header:#10a778,marker:#cc6666,prompt:#81a2be,spinner:#f0c674"
