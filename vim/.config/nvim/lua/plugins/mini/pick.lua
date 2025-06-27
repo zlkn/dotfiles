ef92ba713dca604386ebce522946fb08f5e96fe2
zolkin.u.a <zolkin.u.a@gmail.com>
2025-06-27 11:21:19 +0300

    WIP

--- vim/.config/nvim/lua/plugins/mini/pick.lua
+++ vim/.config/nvim/lua/plugins/mini/pick.lua
@@ -33,20 +33,73 @@ MiniDeps.later(function()
     vim.keymap.set("n", "<leader>pp", ":echo 'Not Implemeted'<CR>", { desc = "Mock for jq over current buffer" })
 end)
 
+vim.api.nvim_set_hl(0, "LspInlayHint", { bg = "red", fg = "blue" })
+
 M = {}
+M.yaml_jump_to_node = function(path)
+    local ok_parser, parser = pcall(vim.treesitter.get_parser, 0, "yaml")
+    if not ok_parser or not parser then
+        print("Could not get parser for YAML")
+        return nil
+    end
+
+    local trees = parser:parse()
+    if not trees or not trees[1] then
+        print("No trees found")
+        return nil
+    end
+
+    local tree = trees[1]
+    local node = tree:root()
+    if not node then
+        print("No root node found")
+        return nil
+    end
+
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    -- iterate each segment in the dotted path
+    for part in path:gmatch("[^.]+") do
+        local found = nil
+
+        for child in node:iter_children() do
+            print("NodeName: " .. vim.treesitter.get_node_text(child, bufnr))
+            if child:named() and child then
+                local txt = vim.treesitter.get_node_text(child, bufnr)
+                if txt == part then
+                    found = child
+                    break
+                end
+            end
+        end
+
+        if not found then
+            vim.notify(("Couldn’t find “%s” under %s"):format(part, node:type()), vim.log.levels.WARN)
+            return
+        end
+
+        node = found
+    end
+
+    print("Find position for Node" .. node)
+
+    local ts_utils = require("nvim-treesitter.ts_utils")
+    ts_utils.goto_node(node, true)
+end
+
 M.yaml_keytrail = function(_, opts)
     local items = require("test").get_all_paths()
     table.sort(items)
 
     local function choose(item)
         print("Choose item: " .. item)
+        M.yaml_jump_to_node(item)
     end
 
     local function preview(bufnr, item)
         return nil
     end
 
-    -- 4. Assemble source
     local source = {
         name = "YamlKeytrail",
         items = items,
