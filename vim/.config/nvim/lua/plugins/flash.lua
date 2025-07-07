3dc90b8b7679777538c5167bff9d50324d8cc858
zolkin.u.a <zolkin.u.a@gmail.com>
2025-07-07 13:00:53 +0300

    feat(nvim) fix flash keymaps overlapping

--- vim/.config/nvim/lua/plugins/flash.lua
+++ vim/.config/nvim/lua/plugins/flash.lua
@@ -1,6 +1,13 @@
 MiniDeps.add({ source = "folke/flash.nvim" })
 MiniDeps.later(function()
-    vim.keymap.set("n", "f", function()
+    require("flash").setup({
+        modes = {
+            char = {
+                enabled = false, -- Disable f, F, t, T motion
+            },
+        },
+    })
+    vim.keymap.set({ "n", "x", "o" }, "f", function()
         require("flash").jump()
     end, { desc = "Start flash jump mode", noremap = true, silent = true })
 end)
