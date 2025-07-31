9f49950d7dae64c80d9a6015f32a1cbda1eab289
zolkin.u.a <zolkin.u.a@gmail.com>
2025-07-31 10:14:40 +0300

    feature(nvim) use k8s schema for any yaml file as default

--- vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -23,7 +23,8 @@ MiniDeps.now(function()
                     url = "",
                 },
                 schemas = vim.tbl_deep_extend("force", require("schemastore").yaml.schemas(), {
-                    kubernetes = { "k8s**.yaml", "kube*/*.yaml", "tshoot*/*.yaml" },
+                    -- kubernetes = { "k8s**.yaml", "kube*/*.yaml", "tshoot*/*.yaml" },
+                    kubernetes = { "*.yaml" },
                     ["https://json.schemastore.org/kustomization.json"] = "kustomization.{yml,yaml}",
                     ["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "docker-compose*.{yml,yaml}",
                     ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"] = "*.Application.yaml",
