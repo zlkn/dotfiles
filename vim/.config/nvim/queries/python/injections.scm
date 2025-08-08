ecf11660079ffef6618bbc236874936e7a7879f2
zolkin.u.a <zolkin.u.a@gmail.com>
2025-08-08 13:49:12 +0300

    feature(nvim) Add python query for language injection

--- vim/.config/nvim/queries/python/injections.scm
+++ vim/.config/nvim/queries/python/injections.scm
@@ -10,4 +10,14 @@
         (string_content) @injection.content
         (string_end)))))
 
+( (comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  .
+  (expression_statement
+    (assignment
+      left: (identifier)
+      right: (string
+        (string_start)
+        (string_content) @injection.content
+        (string_end)))) )
 
