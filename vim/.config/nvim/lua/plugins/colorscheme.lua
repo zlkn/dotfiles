6dd23a8589326a7fbb12279b2cbf7bdc58d63345
zolkin.u.a <zolkin.u.a@gmail.com>
2025-09-10 13:45:10 +0300

    feat(nvim) Migrate colorscheme to palette

--- vim/.config/nvim/lua/plugins/colorscheme.lua
+++ vim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,42 @@
 MiniDeps.add({
     source = "projekt0n/github-nvim-theme",
     name = "github-nvim-theme",
-    hooks = {
-        post_checkout = function()
-            vim.cmd([[ colorscheme github_light ]])
-        end,
-    },
 })
 
 MiniDeps.now(function()
+    local palette = {
+        ansi = {
+            black = "#424242",
+            red = "#c30771",
+            green = "#0d844c",
+            yellow = "#a66f00",
+            blue = "#015692",
+            magenta = "#523c79",
+            cyan = "#008ec4",
+            white = "#9d9d9d",
+        },
+        brights = {
+            black = "#1f1f1f",
+            red = "#fb007a",
+            green = "#10b97a",
+            yellow = "#ee9900",
+            blue = "#0550ae",
+            magenta = "#871094",
+            cyan = "#1fbdd0",
+            white = "#57606a",
+        },
+        extra = {
+            darkBlue = "#0a3069",
+            teal = "#004c63",
+            tirquose = "#007474",
+            cornflowerBlue = "#b6d6fd",
+            gray0 = "#dfdfe1",
+            gray1 = "#a1a1a1",
+            cherry = "#980054",
+            -- dep = "#82071e",
+            -- func = "#8430ce",
+        },
+    }
     require("github-theme").setup({
         options = {
             -- Compiled file's destination location
@@ -19,35 +47,37 @@ MiniDeps.now(function()
                 operators = "bold",
                 numbers = "bold",
                 constants = "bold",
+                builtins = "bold",
             },
         },
         specs = {
             github_light = {
                 syntax = {
-                    bracket = "#1f1f1f",
-                    builtin0 = "#980054",
-                    builtin1 = "#015692",
-                    builtin2 = "#8a4794",
-                    comment = "#57606a",
-                    conditional = "#fb007a",
-                    -- const = "#015692",
-                    const = "#523c79",
-                    dep = "#82071e",
-                    field = "#004c63",
-                    func = "#871094",
-                    -- func = "#8430ce",
-                    ident = "#424242",
-                    keyword = "#0550ae",
-                    number = "#424242",
-                    operator = "#0a3069",
-                    param = "#007474",
-                    preproc = "#c30771",
-                    regex = "#0a3069",
-                    statement = "#fb007a",
-                    string = "#0d844c",
-                    tag = "#015692",
-                    type = "#1f1f1f",
-                    variable = "#424242",
+                    bracket = palette.brights.black,
+                    -- builtin0 = palette.ansi.red,
+                    builtin0 = palette.ansi.magenta,
+                    builtin1 = palette.ansi.blue,
+                    builtin2 = palette.brights.magenta,
+                    comment = palette.brights.white,
+                    conditional = palette.brights.red,
+                    -- const = palette.ansi.blue,
+                    -- const = palette.ansi.magenta,
+                    const = palette.brights.white,
+                    dep = palette.brights.cyan,
+                    field = palette.extra.teal,
+                    func = palette.brights.magenta,
+                    ident = palette.ansi.black,
+                    keyword = palette.brights.blue,
+                    number = palette.ansi.black,
+                    operator = palette.extra.darkBlue,
+                    param = palette.extra.tirquose,
+                    preproc = palette.ansi.red,
+                    regex = palette.extra.darkBlue,
+                    statement = palette.brights.red,
+                    string = palette.ansi.green,
+                    tag = palette.ansi.blue,
+                    type = palette.brights.black,
+                    variable = palette.ansi.black,
                 },
                 diag = {
                     hint = "#1fbdd0",
@@ -57,40 +87,44 @@ MiniDeps.now(function()
     })
 
     vim.cmd([[ colorscheme github_light ]])
-    vim.api.nvim_set_hl(0, "CursorLine", { bg = "#dfdfe1" })
-    vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "#dfdfe1" })
-    vim.api.nvim_set_hl(0, "CursorLineSign", { bg = "#dfdfe1" })
 
-    vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", { bg = "#b6d6fd" })
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = palette.extra.gray0 })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { bg = palette.extra.gray0 })
+    vim.api.nvim_set_hl(0, "CursorLineSign", { bg = palette.extra.gray0 })
+
+    vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", { bg = palette.extra.cornflowerBlue })
+    -- vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", { underline = true, sp = palette.brights.cyan })
+    -- vim.api.nvim_set_hl(0, "MiniCursorword", { underline = true, sp = palette.brights.cyan })
 
     -- Transparent/“inherit terminal” bg for builtin tabline
-    vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
-    vim.api.nvim_set_hl(0, "TabLine", { bg = "NONE", fg = "#888888" })
-    vim.api.nvim_set_hl(0, "TabLineSel", { bg = "NONE", fg = "#ffffff", bold = true })
-
-    vim.cmd([[ hi FloatBorder guifg=#a1a1a1 guibg=none]])
-    vim.cmd([[ hi MiniPickPromptCaret guifg=#a1a1a1 guibg=none ]])
-    vim.cmd([[ hi MiniPickPromptPrefix guifg=#a1a1a1 guibg=none]])
-    vim.cmd([[ hi MiniPickPrompt guifg=#424242 guibg=none]])
-
-    -- force transparent background for all themes
-    vim.cmd([[
-        highlight Normal guibg=none
-        highlight NonText guibg=none
-        highlight Normal ctermbg=none
-        highlight NonText ctermbg=none
-    ]])
-    -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "", fg = "" })
+    vim.api.nvim_set_hl(0, "TabLineFill", { bg = none })
+    -- vim.api.nvim_set_hl(0, "TabLine", { bg = none, fg = "#888888" })
+    -- vim.api.nvim_set_hl(0, "TabLineSel", { bg = none, fg = "#ffffff", bold = true })
 
     -- Minipick
-    vim.cmd([[ hi MiniPickMatchRanges guifg=#871094 guibg=none]])
+    vim.api.nvim_set_hl(0, "MiniPickMatchRanges", { fg = palette.brights.magenta })
+    vim.api.nvim_set_hl(0, "MiniPickPromptCaret", { fg = palette.extra.gray1 })
+    vim.api.nvim_set_hl(0, "MiniPickPromptPrefix", { fg = palette.extra.gray1 })
+    vim.api.nvim_set_hl(0, "MiniPickPrompt", { fg = palette.ansi.black })
 
-    vim.api.nvim_set_hl(0, "@constructor", { fg = "#980054" })
+    -- All floating window border
+    vim.api.nvim_set_hl(0, "FloatBorder", { fg = palette.extra.gray1, bg = none })
+
+    -- -- force transparent background for all themes
+    -- vim.cmd([[
+    --     highlight Normal guibg=none
+    --     highlight NonText guibg=none
+    --     highlight Normal ctermbg=none
+    --     highlight NonText ctermbg=none
+    -- ]])
+    -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "", fg = "" })
+
+    vim.api.nvim_set_hl(0, "@constructor", { fg = palette.extra.cherry })
 
     --  ansible_semantic_highlight
-    vim.api.nvim_set_hl(0, "@lsp.type.class", { fg = "#980054" })
-    vim.api.nvim_set_hl(0, "@lsp.type.method.yaml.ansible", { fg = "#004c63" })
+    vim.api.nvim_set_hl(0, "@lsp.type.class", { fg = palette.extra.cherry })
+    vim.api.nvim_set_hl(0, "@lsp.type.method.yaml.ansible", { fg = palette.extra.tirquose })
 
     -- golang semantic highlight
-    vim.api.nvim_set_hl(0, "@keyword.function", { fg = "#015692", bold = true })
+    vim.api.nvim_set_hl(0, "@keyword.function", { fg = palette.ansi.blue, bold = true })
 end)
--- vim/.config/nvim/lua/plugins/rainbow-delimiter.lua
+++ vim/.config/nvim/lua/plugins/rainbow-delimiter.lua
@@ -1,22 +1,35 @@
 MiniDeps.add("HiPhish/rainbow-delimiters.nvim")
 MiniDeps.later(function()
-    require("rainbow-delimiters.setup").setup({
-        highlight = {
-            "RainbowDelimiterBlack",
-            "RainbowDelimiterRed",
-            "RainbowDelimiterYellow",
-            "RainbowDelimiterBlue",
-            "RainbowDelimiterOrange",
-            "RainbowDelimiterGreen",
-            "RainbowDelimiterViolet",
-            "RainbowDelimiterCyan",
-            -- "RainbowRed",
-            -- "RainbowYellow",
-            -- "RainbowBlue",
-            -- "RainbowOrange",
-            -- "RainbowGreen",
-            -- "RainbowViolet",
-            -- "RainbowCyan",
-        },
-    })
+    local suite_all_black = {
+
+        "RainbowDelimiterBlack",
+        "RainbowDelimiterBlack",
+        "RainbowDelimiterBlack",
+        "RainbowDelimiterBlack",
+        "RainbowDelimiterBlack",
+        "RainbowDelimiterBlack",
+        "RainbowDelimiterBlack",
+        "RainbowDelimiterBlack",
+    }
+    local suite_rainbow = {
+        "RainbowDelimiterBlack",
+        "RainbowDelimiterRed",
+        "RainbowDelimiterYellow",
+        "RainbowDelimiterBlue",
+        "RainbowDelimiterOrange",
+        "RainbowDelimiterGreen",
+        "RainbowDelimiterViolet",
+        "RainbowDelimiterCyan",
+    }
+    local rd = require("rainbow-delimiters.setup")
+
+    function _G.ToogleRainbowDelimiters()
+        if vim.g.rainbow_delimiters_enabled then
+            rd.setup({ highlight = suite_all_black })
+            vim.g.rainbow_delimiters_enabled = false
+        else
+            rd.setup({ highlight = suite_rainbow })
+            vim.g.rainbow_delimiters_enabled = true
+        end
+    end
 end)
