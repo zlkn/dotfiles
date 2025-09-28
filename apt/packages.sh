a2ac5ecbd8c2e1245a922924c3af1ec116b7d64d
zolkin.u.a <zolkin.u.a@gmail.com>
2025-09-28 19:20:38 +0300

    feature(apt) add jetbrains repository

--- apt/packages.sh
+++ apt/packages.sh
@@ -63,11 +63,17 @@ docker(){
   apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
-gitlab
-wezterm
-fish
-kubectl
-gcloud
-hashicorp
+jetbrains(){
+  curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | gpg --dearmor | sudo tee /usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg > /dev/null
+  echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com any main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
+  sudo apt update
+}
 
+# gitlab
+# wezterm
+# fish
+# kubectl
+# gcloud
+# hashicorp
+jetbrains
 
