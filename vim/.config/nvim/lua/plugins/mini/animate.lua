5d33bd6a4df06fc00c43435c469184dd22026072
zolkin.u.a <zolkin.u.a@gmail.com>
2025-11-11 16:10:46 +0200

    feat(nvim) rearrange code

--- vim/.config/nvim/lua/plugins/mini/animate.lua
+++ vim/.config/nvim/lua/plugins/mini/animate.lua
@@ -1,18 +1,13 @@
-MiniDeps.add("nvim-mini/mini.animate")
+-- MiniDeps.add("nvim-mini/mini.animate")
 MiniDeps.later(function()
     local animate = require("mini.animate")
     animate.setup({
-        scroll = {
-            -- Disable Scroll Animations, as the can interfer with mouse Scrolling
-            enable = false,
-        },
+        cursor = { enable = true },
+        -- Disable Scroll Animations, as the can interfer with mouse Scrolling
+        scroll = { enable = false },
         -- Disable window animation due blinking background
         open = { enable = false },
         close = { enable = false },
         resize = { enable = false },
-
-        cursor = {
-            timing = animate.gen_timing.cubic({ duration = 200, unit = "total" }),
-        },
     })
 end)
