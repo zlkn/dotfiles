7491970cd23e7f1036a5fbd2a0ab05b2be45e05a
zolkin.u.a <zolkin.u.a@gmail.com>
2025-11-13 14:24:08 +0200

    chore(nvim) delete unused codecompanion plugin

--- vim/.config/nvim/lua/plugins/codecompanion.lua
+++
@@ -1,8 +0,0 @@
-MiniDeps.add({ source = "olimorris/codecompanion.nvim", depends = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" } })
-MiniDeps.later(function()
-    require("codecompanion").setup({
-        strategies = {
-            chat = { adapter = "copilot" },
-        },
-    })
-end)
