ed863c6b24206c9ba30d283ccec9c35cbf9d35fd
zolkin.u.a <zolkin.u.a@gmail.com>
2025-11-13 12:30:50 +0200

    feat(wezterm) new keybinding for ShowTabNavigator

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -212,7 +212,7 @@ end)
 -- Keybindings
 config.leader = { key = "RightAlt", mods = "NONE", timeout_milliseconds = 1000 }
 config.keys = {
-    { key = "l", mods = "CTRL|SHIFT", action = wezterm.action.ShowTabNavigator },
+    { key = "`", mods = "CTRL", action = wezterm.action.ShowTabNavigator },
     { key = "UpArrow", mods = "SHIFT", action = wezterm.action.ScrollToPrompt(-1) },
     { key = "DownArrow", mods = "SHIFT", action = wezterm.action.ScrollToPrompt(1) },
     { key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
