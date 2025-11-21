215a050397ca2f737229c734f8d015bf84b4e071
zolkin.u.a <zolkin.u.a@gmail.com>
2025-11-21 12:38:01 +0200

    feat(nvim) disable noizy virual lines

--- vim/.config/nvim/lua/options.lua
+++ vim/.config/nvim/lua/options.lua
@@ -44,9 +44,7 @@ vim.opt.guicursor = {
 }
 
 vim.diagnostic.config({
-    virtual_lines = {
-        current_line = true,
-    },
+    virtual_lines = false,
     underline = true,
     float = {
         -- UI.
