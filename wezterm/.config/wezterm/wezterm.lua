99307f21e986d8138b9afe4a4a68ec9ff7f47fc9
zolkin.u.a <zolkin.u.a@gmail.com>
2025-12-09 22:59:44 +0200

    feat(wezterm) user brighter colors and light font rendering

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -14,7 +14,7 @@ config.inactive_pane_hsb = {
 
 -- Fontconfig
 local font_size = 11
-local font = { family = "JetBrains Mono", weight = "Regular" }
+local font = { family = "JetBrains Mono", weight = "Light" }
 config.font = wezterm.font(font)
 config.warn_about_missing_glyphs = false
 config.font_size = font_size
