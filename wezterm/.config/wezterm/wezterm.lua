f284b3d1e0bb1ad8c3929b907251204cd3c81185
zolkin.u.a <zolkin.u.a@gmail.com>
2026-02-12 14:06:32 +0200

    chore(wezterm) bring some logical structure into config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,11 +9,6 @@ config.automatically_reload_config = false
 config.warn_about_missing_glyphs = false
 config.window_close_confirmation = "NeverPrompt"
 
--- Colorscheme
-local colorscheme = require("colors")
-config.colors = colorscheme.colors
-config.default_cursor_style = "BlinkingBar"
-
 -- Fontconfig
 local font_size = 11
 local font = { family = "JetBrains Mono", weight = "Light" }
@@ -21,9 +16,14 @@ config.font = wezterm.font(font)
 config.font_size = font_size
 config.bold_brightens_ansi_colors = false
 
+-- Colorscheme
+local colorscheme = require("colors")
+config.colors = colorscheme.colors
+config.default_cursor_style = "BlinkingBar"
+
 require("wayland_gnome").apply_to_config(config)
 require("keys").apply_to_config(config)
-require("appearance").apply(config, colorscheme, font, font_size)
+-- require("appearance").apply(config, colorscheme, font, font_size)
 require("format_tab_tittle")
 
 --- Right status bar
