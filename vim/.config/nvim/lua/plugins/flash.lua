8feeaf8aea9125d2e7404854068a8c294ef2061b
zolkin.u.a <zolkin.u.a@gmail.com>
2026-02-19 12:17:16 +0200

    chore(nvim) flash config and keymap

--- vim/.config/nvim/lua/plugins/flash.lua
+++ vim/.config/nvim/lua/plugins/flash.lua
@@ -2,17 +2,15 @@ MiniDeps.add({ source = "folke/flash.nvim" })
 MiniDeps.later(function()
     require("flash").setup({
         modes = {
-            -- search = {
-            --     enabled = true,
-            -- },
             char = {
                 enabled = false, -- Disable f, F, t, T motion
             },
         },
     })
-    vim.keymap.set({ "n", "x", "o" }, "f", function()
+    vim.keymap.set({ "n", "x", "o" }, "<CR>", function()
         require("flash").jump()
     end, { desc = "Start flash jump mode", noremap = true, silent = true })
+
     vim.keymap.set({ "n", "x", "o" }, "<c-space>", function()
         require("flash").treesitter({
             actions = {
