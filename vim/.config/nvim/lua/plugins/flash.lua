4e24db3da533fea3b2be49f104c1edb06372d4e4
zolkin.u.a <zolkin.u.a@gmail.com>
2026-02-20 07:44:08 +0200

    chore(nvim) disable inc treesiter select via flash

--- vim/.config/nvim/lua/plugins/flash.lua
+++ vim/.config/nvim/lua/plugins/flash.lua
@@ -3,7 +3,7 @@ MiniDeps.later(function()
     require("flash").setup({
         modes = {
             char = {
-                enabled = false, -- Disable f, F, t, T motion
+                enabled = true, -- Disable f, F, t, T motion
             },
         },
     })
@@ -11,12 +11,12 @@ MiniDeps.later(function()
         require("flash").jump()
     end, { desc = "Start flash jump mode", noremap = true, silent = true })
 
-    vim.keymap.set({ "n", "x", "o" }, "<c-space>", function()
-        require("flash").treesitter({
-            actions = {
-                ["<c-space>"] = "next",
-                ["<BS>"] = "prev",
-            },
-        })
-    end, { desc = "Treesitter incremental selection" })
+    -- vim.keymap.set({ "n", "x", "o" }, "<c-space>", function()
+    --     require("flash").treesitter({
+    --         actions = {
+    --             ["<c-space>"] = "next",
+    --             ["<BS>"] = "prev",
+    --         },
+    --     })
+    -- end, { desc = "Treesitter incremental selection" })
 end)
