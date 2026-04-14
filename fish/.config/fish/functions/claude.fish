b259e2664d539f3cfa5df222d740d8865b747887
zolkin.u.a <zolkin.u.a@gmail.com>
2026-04-14 16:04:00 +0300

    feat(fish) more specific run for claude

---
+++ fish/.config/fish/functions/claude.fish
@@ -0,0 +1,9 @@
+function claude
+    if command -q pass
+        set -x ANTHROPIC_API_KEY (pass tokens/ANTHROPIC_API_KEY)
+    else
+        echo "pass is not installed, cannot get ANTHROPIC_API_KEY"
+        return 1
+    end
+    command claude $argv
+end
