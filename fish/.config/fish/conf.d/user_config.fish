f5437ef784772b5cfdcf069e9a18c60c0f947cd6
zolkin.u.a <zolkin.u.a@gmail.com>
2026-04-14 15:23:27 +0300

    feat(fish) store claude key in password manager

--- fish/.config/fish/conf.d/user_config.fish
+++ fish/.config/fish/conf.d/user_config.fish
@@ -6,6 +6,13 @@ set -g -x USE_GKE_GCLOUD_AUTH_PLUGIN True
 set -gx ANTHROPIC_API_KEY (python3 -c "import json; print(json.load(open('$HOME/.claude.json'))['primaryApiKey'])" 2>/dev/null)
 set -gx HELIX_RUNTIME "$HOME/dotfiles/helix/.config/helix/runtime"
 
+if command -q pass
+    set -gx ANTHROPIC_API_KEY (pass show tokens/ANTHROPIC_API_KEY)
+else
+    echo "WARNING: 'pass' is not installed. Secret tokens for session not configured"
+end
+
+
 function __fzf_history_search
     commandline (history | fzf)
     commandline --function --replace
