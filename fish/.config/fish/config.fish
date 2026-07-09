c77810e9aa52557c7b42abcffc309c0964573d66
zolkin.u.a <zolkin.u.a@gmail.com>
2026-07-10 01:40:28 +0300

    feat(fish) PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,7 @@ fish_add_path --prepend /opt/pipx/venvs/ansible/bin
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/yzolkin/.local/bin" $PATH
