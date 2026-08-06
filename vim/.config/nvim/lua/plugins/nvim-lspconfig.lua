c61a84bea81724bc46c91b3f730004398fae8641
zolkin.u.a <zolkin.u.a@gmail.com>
2026-08-06 10:55:40 +0300

    feat(nvim) add lsp table and option table
    Now easy to find enabled lsp's

--- vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,146 +1,134 @@
 MiniDeps.add({ source = "neovim/nvim-lspconfig", depends = { "b0o/SchemaStore.nvim" } })
 MiniDeps.now(function()
-    vim.lsp.enable("helm_ls")
-    vim.lsp.config("helm_ls", {})
-
-    vim.lsp.enable("yamlls")
-    vim.lsp.config("yamlls", {
-        filetypes = { "yaml", "yml", "yaml.docker-compose", "yaml.ansible" },
-        settings = {
-            redhat = { telemetry = { enabled = false } },
-            yaml = {
-                keyOrdering = false,
-                format = {
-                    enable = false,
-                },
-                completion = true,
-                validate = true,
-                hover = true,
-                schemaStore = {
-                    -- Must disable built-in schemaStore support to use
-                    -- schemas from SchemaStore.nvim plugin
-                    enable = false,
-                    -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-                    url = "",
-                },
-                schemas = vim.tbl_deep_extend("force", require("schemastore").yaml.schemas(), {
-                    ["https://json.schemastore.org/kustomization.json"] = "kustomization.{yml,yaml}",
-                    ["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "docker-compose*.{yml,yaml}",
-                    ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"] = "*.Application.yaml",
-                    ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/applicationset_v1alpha1.json"] = "*.ApplicationSet.yaml",
-                    ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
-                    kubernetes = {
-                        "*.DaemonSet.yaml",
-                        "*.Deployment.yaml",
-                        "*.StatefulSet.yaml",
-                        "*.ReplicaSet.yaml",
-                        "*.Service.yaml",
-                        "*.Pod.yaml",
-                        "*.Namespace.yaml",
-                        "*.ConfigMap.yaml",
-                        "*.Secret.yaml",
-                        "*.Job.yaml",
-                        "*.CronJob.yaml",
-                        "*.PersistentVolume.yaml",
-                        "*.PersistentVolumeClaim.yaml",
-                        "*.StorageClass.yaml",
-                        "*.Ingress.yaml",
-                        "*.NetworkPolicy.yaml",
-                        "*.ServiceAccount.yaml",
-                        "*.Role.yaml",
-                        "*.RoleBinding.yaml",
-                        "*.ClusterRole.yaml",
-                        "*.ClusterRoleBinding.yaml",
+    local options = {
+        default = {},
+        ansiblels = {
+            cmd = { "ansible-language-server", "--stdio" },
+            settings = {
+                ansible = {
+                    python = { interpreterPath = "python3" },
+                    ansible = { path = "ansible" },
+                    executionEnvironment = { enabled = false },
+                    validation = {
+                        enabled = false,
+                        lint = { enabled = true, path = "ansible-lint" },
                     },
-                }),
+                },
             },
-        },
-    })
+            filetypes = { "yaml.ansible" },
+            root_markers = { "ansible.cfg", ".ansible-lint" },
+            single_file_support = false,
 
-    vim.lsp.enable("lua_ls")
-    vim.lsp.config("lua_ls", {
-        filetypes = { "lua" },
-        on_init = function(client)
-            if client.workspace_folders then
-                local path = client.workspace_folders[1].name
-                if path ~= vim.fn.stdpath("config") and vim.uv.fs_stat(path .. "/.luarc.json") then
-                    return
+        },
+        terraformls = {
+            cmd = { "terraform-ls", "serve" },
+            filetypes = { "terraform", "tf" },
+            root_markers = { ".terraform", ".git" },
+        },
+        luals = {
+            filetypes = { "lua" },
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if path ~= vim.fn.stdpath("config") and vim.uv.fs_stat(path .. "/.luarc.json") then
+                        return
+                    end
                 end
-            end
 
-            client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                runtime = {
-                    -- Tell the language server which version of Lua you're using
-                    -- (most likely LuaJIT in the case of Neovim)
-                    version = "LuaJIT",
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                    checkThirdParty = true,
-                    library = {
-                        vim.env.VIMRUNTIME,
-                        -- Depending on the usage, you might want to add additional paths here.
-                        "${3rd}/luv/library",
-                        -- "${3rd}/busted/library",
+                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = "LuaJIT",
                     },
-                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-                    -- library = vim.api.nvim_get_runtime_file("", true)
-                },
-            })
-        end,
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = true,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                            -- Depending on the usage, you might want to add additional paths here.
+                            "${3rd}/luv/library",
+                            -- "${3rd}/busted/library",
+                        },
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    },
+                })
+            end,
 
-        settings = {
-            Lua = {
-                hint = { enable = true },
-                diagnostics = {
-                    globals = { "vim", "MiniDeps", "MiniPick", "MiniFiles", "none", "MiniExtra" },
-                },
-            },
-        },
-    })
-
-    vim.lsp.enable("tflint")
-    vim.lsp.config("tflint", {
-        filetypes = { "terraform", "tf" },
-        root_markers = { ".terraform", ".git" },
-    })
-    vim.lsp.enable("terraformls")
-    vim.lsp.config("terraformls", {
-        cmd = { "terraform-ls", "serve" },
-        filetypes = { "terraform", "tf" },
-        root_markers = { ".terraform", ".git" },
-    })
-
-    vim.lsp.enable("ansiblels")
-    vim.lsp.config("ansiblels", {
-        cmd = { "ansible-language-server", "--stdio" },
-        settings = {
-            ansible = {
-                python = { interpreterPath = "python3" },
-                ansible = { path = "ansible" },
-                executionEnvironment = { enabled = false },
-                validation = {
-                    enabled = false,
-                    lint = { enabled = true, path = "ansible-lint" },
+            settings = {
+                Lua = {
+                    hint = { enable = true },
+                    diagnostics = {
+                        globals = { "vim", "MiniDeps", "MiniPick", "MiniFiles", "none", "MiniExtra" },
+                    },
                 },
             },
+
         },
-        filetypes = { "yaml.ansible" },
-        root_markers = { "ansible.cfg", ".ansible-lint" },
-        single_file_support = false,
-    })
+        yamlls = {
+            filetypes = { "yaml", "yml", "yaml.docker-compose", "yaml.ansible" },
+            settings = {
+                redhat = { telemetry = { enabled = false } },
+                yaml = {
+                    keyOrdering = false,
+                    format = {
+                        enable = false,
+                    },
+                    completion = true,
+                    validate = true,
+                    hover = true,
+                    schemaStore = {
+                        -- Must disable built-in schemaStore support to use
+                        -- schemas from SchemaStore.nvim plugin
+                        enable = false,
+                        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                        url = "",
+                    },
+                    schemas = vim.tbl_deep_extend("force", require("schemastore").yaml.schemas(), {
+                        ["https://json.schemastore.org/kustomization.json"] = "kustomization.{yml,yaml}",
+                        ["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "docker-compose*.{yml,yaml}",
+                        ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"] = "*.Application.yaml",
+                        ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/applicationset_v1alpha1.json"] = "*.ApplicationSet.yaml",
+                        ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+                        kubernetes = {
+                            "*.DaemonSet.yaml",
+                            "*.Deployment.yaml",
+                            "*.StatefulSet.yaml",
+                            "*.ReplicaSet.yaml",
+                            "*.Service.yaml",
+                            "*.Pod.yaml",
+                            "*.Namespace.yaml",
+                            "*.ConfigMap.yaml",
+                            "*.Secret.yaml",
+                            "*.Job.yaml",
+                            "*.CronJob.yaml",
+                            "*.PersistentVolume.yaml",
+                            "*.PersistentVolumeClaim.yaml",
+                            "*.StorageClass.yaml",
+                            "*.Ingress.yaml",
+                            "*.NetworkPolicy.yaml",
+                            "*.ServiceAccount.yaml",
+                            "*.Role.yaml",
+                            "*.RoleBinding.yaml",
+                            "*.ClusterRole.yaml",
+                            "*.ClusterRoleBinding.yaml",
+                        },
+                    }),
+                },
+            },
+        }
+    }
 
-    vim.lsp.enable("dockerls")
-    vim.lsp.config("dockerls", {})
+    local enabled_lsp = {"yamlls", "helm_ls", "luals", "dockerls", "ruff", "gopls", "ansiblels"}
 
-    vim.lsp.enable("ruff")
-    vim.lsp.config("ruff", {})
+    for _, lsp in ipairs(enabled_lsp) do
 
-    -- vim.lsp.enable("basedpyright")
-    -- vim.lsp.config("basedpyright", {})
+        local opts = options[lsp] or options.default
+        -- print("options: " .. vim.inspect(opts))
 
-    vim.lsp.enable("ruff", {})
-    vim.lsp.config("ruff", {})
-    vim.lsp.enable("gopls")
-    vim.lsp.config("gopls", {})
+        vim.lsp.config(lsp, opts)
+        vim.lsp.enable(lsp)
+    end
+
 end)
