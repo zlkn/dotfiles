2739d8b02ff4392b1ce3496981354a7ee678b1af
zolkin.u.a <zolkin.u.a@gmail.com>
2026-09-05 06:51:57 +0300

    chore(finsh) migrate to v4

--- fish/.config/fish/conf.d/user_config.fish
+++ fish/.config/fish/conf.d/user_config.fish
@@ -3,6 +3,9 @@ if not status is-interactive
 end
 
 set -g -x USE_GKE_GCLOUD_AUTH_PLUGIN True
+# set -g -x GOPATH /usr/local/go/bin
+set -g -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -g -x CMD_COUNT 0
 
 function __fish_clear_buffer
     for line in (seq 2 (tput lines))
