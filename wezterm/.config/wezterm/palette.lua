61cef08bf11b08be303c38270b914bdaa8cffb66
zolkin.u.a <zolkin.u.a@gmail.com>
2026-09-15 22:43:13 +0300

    feat(palette) make white and black colors more faancy

--- wezterm/.config/wezterm/palette.lua
+++ wezterm/.config/wezterm/palette.lua
@@ -1,12 +1,21 @@
 local wezterm = require("wezterm")
 
+-- M.variant = {
+--     palette = "day",
+--     font = {
+--         weight = "Regular"
+--     }
+-- }
+
 local appearance = "Dark"
 if wezterm.gui then
     appearance = wezterm.gui.get_appearance()
 end
 
 if appearance:find("Dark") then
-    return require("palette_aqua_night")
+    return require( "palette_aqua_night")
 end
 return require("palette_aqua_day")
+
+-- return M.variant
 
